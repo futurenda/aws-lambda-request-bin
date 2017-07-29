84edb7bd9621779ab6b9afa3928c8704cc9e150a
Zeno Zeng <zenoofzeng@gmail.com>
2017-07-29 13:22:43 +0800

    parse body

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 exports.handler = (event, context, callback) => {
+    try {
+        event.body = JSON.parse(event.body);
+    } catch (e) {
+    }
     console.log(JSON.stringify(event, null, 4));
     callback(null, {statusCode: 200});
 };
